8ef6a2027ae81f14db9273c65080ac6f0412e3c0
JEscGon <JesusE.G@outlook.es>
2025-03-12 14:26:22 +0100

    1rst Commit

---
+++ src/test/java/com/dev/insurance_policies/InsurancePoliciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.insurance_policies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePoliciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
